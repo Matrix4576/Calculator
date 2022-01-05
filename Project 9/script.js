b8232f8e8a9cb24b3728a16fae2231eb3c7b547c
Agniva Roy <77002089+Matrix4576@users.noreply.github.com>
2022-01-05 14:56:34 +0530

    Added the main files

---
+++ Project 9/script.js
@@ -0,0 +1,25 @@
+function myfunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+}
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
